e05bea5417da0fb6996897353d0ecc213237d4c8
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-14 17:51:32 -0700

    pass submit handler as props from App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import '../css/App.css';
 
-const App = () => {
-  return (
-    <div className='ui container'>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  onSearchSubmit = term => {
+    console.log(term);
+  };
+
+  render() {
+    return (
+      <div className='ui container'>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,7 @@ class SearchBar extends Component {
 
   onFormSubmit = event => {
     event.preventDefault();
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
